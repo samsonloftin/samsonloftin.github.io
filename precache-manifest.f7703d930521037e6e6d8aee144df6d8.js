b07564377a68c421b28787174874ff83ab2deb5d
Samson <47623975+samsonloftin@users.noreply.github.com>
2019-07-27 02:07:14 -0700

    Updates

---
+++ precache-manifest.f7703d930521037e6e6d8aee144df6d8.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3da0ad4560cb726aa7cfca268059ecc",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e3948138d09c584790cf",
+    "url": "/portfolio/static/css/main.17ab8daa.chunk.css"
+  },
+  {
+    "revision": "e5b709d2c47869830cec",
+    "url": "/portfolio/static/js/2.0c062922.chunk.js"
+  },
+  {
+    "revision": "e3948138d09c584790cf",
+    "url": "/portfolio/static/js/main.a6d74442.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "06f62336d64c892aa0a0470ffdba2c58",
+    "url": "/portfolio/static/media/art-demo.06f62336.jpg"
+  },
+  {
+    "revision": "f1402aa9e81e4cd154b8a88ebf5d12dd",
+    "url": "/portfolio/static/media/art-dragoneaters.f1402aa9.png"
+  },
+  {
+    "revision": "c9efa23c5da147afd3813887e009f063",
+    "url": "/portfolio/static/media/art-gen.c9efa23c.jpg"
+  },
+  {
+    "revision": "ee10c06772da4bcd75cce0321d23ea44",
+    "url": "/portfolio/static/media/art-jak.ee10c067.png"
+  },
+  {
+    "revision": "27fdaf885f7368ad3a3e719f096905be",
+    "url": "/portfolio/static/media/art-mountain.27fdaf88.jpg"
+  },
+  {
+    "revision": "44f05960bb4094076ac796037bd6ff29",
+    "url": "/portfolio/static/media/code-arcadegame.44f05960.jpg"
+  },
+  {
+    "revision": "273c9fa618a4b79bd721325f8a7ffaf1",
+    "url": "/portfolio/static/media/code-booktracker.273c9fa6.jpg"
+  },
+  {
+    "revision": "5341e36b94e18802e8cefc8ed3aac654",
+    "url": "/portfolio/static/media/code-memory.5341e36b.jpg"
+  },
+  {
+    "revision": "06ef6b2a910cb6b28068fb6fc2cd8536",
+    "url": "/portfolio/static/media/code-review.06ef6b2a.jpg"
+  },
+  {
+    "revision": "2586195e2230d214f93d48e15ccd42d9",
+    "url": "/portfolio/static/media/cover-about-1x.2586195e.jpg"
+  },
+  {
+    "revision": "27d7042a383187081d9475647d01789c",
+    "url": "/portfolio/static/media/cover-about-halfx.27d7042a.jpg"
+  },
+  {
+    "revision": "5647f425fd3ba536db3f8b95b14ba4c4",
+    "url": "/portfolio/static/media/design-kitestring.5647f425.jpg"
+  },
+  {
+    "revision": "f0dee57a3e241147de2b562dc5bc3045",
+    "url": "/portfolio/static/media/design-mariaopoly.f0dee57a.jpg"
+  },
+  {
+    "revision": "617870e422d4b75087cdcf9ff298032e",
+    "url": "/portfolio/static/media/github.617870e4.svg"
+  },
+  {
+    "revision": "619231ff8ec7a6b4d39aede30fa9890e",
+    "url": "/portfolio/static/media/in.619231ff.svg"
+  },
+  {
+    "revision": "f7269851a1d8fc41cc78b0c569157ae8",
+    "url": "/portfolio/static/media/logo.f7269851.svg"
+  },
+  {
+    "revision": "d536fb80087d6ea3164e36f7b43ec25c",
+    "url": "/portfolio/static/media/placeholder.d536fb80.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f84ea3c82f1bf6c8313cd02599568aa6.js"
+  "/portfolio/precache-manifest.f7703d930521037e6e6d8aee144df6d8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
