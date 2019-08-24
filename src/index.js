8e7fc7f2c9378d8ab743598a209fa7584d6a5c75
Samson <47623975+samsonloftin@users.noreply.github.com>
2019-08-24 11:22:21 -0700

    feat browserrouter
    I moved from GitHub to Heroku for my portfolio & I'm testing if directs will work again. So I switched away from HashRouter

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
-  <HashRouter basename='/'>
+  <BrowserRouter basename='/'>
     <App />
-  </HashRouter>, document.getElementById('root'));
+  </BrowserRouter>, document.getElementById('root'));
