f699faa04b0997fd5ed6f366ebcefa493644b362
Samson <47623975+samsonloftin@users.noreply.github.com>
2019-08-26 12:59:23 -0700

    Updates

---
+++ precache-manifest.ca4a648deb918e0a5bf4440cdfbe63b9.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07ab2ab224471123bac0d081bfb7be99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d9c69b899c8a74c8d34",
+    "url": "/static/css/main.5eb46db8.chunk.css"
+  },
+  {
+    "revision": "b239a98f3c079b73bdf4",
+    "url": "/static/js/2.ae2bc8bd.chunk.js"
+  },
+  {
+    "revision": "6d9c69b899c8a74c8d34",
+    "url": "/static/js/main.e36f8ac6.chunk.js"
+  },
+  {
+    "revision": "57d6d0088dcaaea56891",
+    "url": "/static/js/runtime~main.3f8956b2.js"
+  },
+  {
+    "revision": "9a0320332d277a09b1016b4046667d1a",
+    "url": "/static/media/10yretro_thumb.9a032033.jpg"
+  },
+  {
+    "revision": "a155c6c1a15ce25daae41efa497f8a1e",
+    "url": "/static/media/10yretro_video01.a155c6c1.mp4"
+  },
+  {
+    "revision": "18f367148ec8cf391a393717959122be",
+    "url": "/static/media/booktracker_thumb.18f36714.jpg"
+  },
+  {
+    "revision": "18c28dcfaf57797d90bc014902eb3750",
+    "url": "/static/media/classcrass_thumb.18c28dcf.jpg"
+  },
+  {
+    "revision": "345eeb4a76e81cecfefc19223c0700ae",
+    "url": "/static/media/classcrassintro.345eeb4a.mp4"
+  },
+  {
+    "revision": "a143ec23c78073d70c30ac8f4a4a28cd",
+    "url": "/static/media/classcrassoutro.a143ec23.mp4"
+  },
+  {
+    "revision": "228e9ff25833c7fe4028aa4c25063422",
+    "url": "/static/media/classicarcadeclassic_thumb.228e9ff2.jpg"
+  },
+  {
+    "revision": "70e49c720063663b572b918b6dda4aa9",
+    "url": "/static/media/cover-about.70e49c72.jpg"
+  },
+  {
+    "revision": "7b4f83af950752a6b9401df574f089cb",
+    "url": "/static/media/dragoneaters_main.7b4f83af.jpg"
+  },
+  {
+    "revision": "bede66a74d8b7d88054c97a957abcfb6",
+    "url": "/static/media/dragoneaters_thumb.bede66a7.jpg"
+  },
+  {
+    "revision": "0c62e08af94b9c51dd0dc1006a94f162",
+    "url": "/static/media/eye_thumb.0c62e08a.jpg"
+  },
+  {
+    "revision": "3523c430297c619d0da2ab2253255682",
+    "url": "/static/media/gen_main.3523c430.jpg"
+  },
+  {
+    "revision": "b29d42ff34efcdac526657ae6ec8869a",
+    "url": "/static/media/gen_main01.b29d42ff.jpg"
+  },
+  {
+    "revision": "0035a2c1c57a11f42056d2be9fc291aa",
+    "url": "/static/media/gen_thumb.0035a2c1.jpg"
+  },
+  {
+    "revision": "4b31452279529c95c387f3967553d1e2",
+    "url": "/static/media/jogha_main01.4b314522.jpg"
+  },
+  {
+    "revision": "a2569442eebe656978f7a62756cbc573",
+    "url": "/static/media/jogha_main02.a2569442.jpg"
+  },
+  {
+    "revision": "5176b5684fc6a589b059989d249fc78a",
+    "url": "/static/media/jogha_main03.5176b568.jpg"
+  },
+  {
+    "revision": "26244cc931a278fc1a471c0e1ac9534b",
+    "url": "/static/media/jogha_main04.26244cc9.jpg"
+  },
+  {
+    "revision": "66f504c17126c43bd8d276c9cc41c37c",
+    "url": "/static/media/jogha_thumb.66f504c1.jpg"
+  },
+  {
+    "revision": "f768d4f0ca2496e91abfab367add0d11",
+    "url": "/static/media/kitestring_thumb.f768d4f0.jpg"
+  },
+  {
+    "revision": "a512e478eb962850ce5663168428e932",
+    "url": "/static/media/ks01.a512e478.jpg"
+  },
+  {
+    "revision": "1c36892bb7fa50066e6dc75392d91e0a",
+    "url": "/static/media/ks02.1c36892b.jpg"
+  },
+  {
+    "revision": "ac7240914284b1c5893ba4536e69e8db",
+    "url": "/static/media/ks03.ac724091.jpg"
+  },
+  {
+    "revision": "d4b05465e31097721eccefab2c85949c",
+    "url": "/static/media/ks04.d4b05465.jpg"
+  },
+  {
+    "revision": "0069cdd9c981eba878d38d56845645e4",
+    "url": "/static/media/ks05.0069cdd9.jpg"
+  },
+  {
+    "revision": "fcdf37a669859854488b5bc041dc25f5",
+    "url": "/static/media/ks06.fcdf37a6.jpg"
+  },
+  {
+    "revision": "322ce8228740999003059311f54fe5e3",
+    "url": "/static/media/ks07.322ce822.jpg"
+  },
+  {
+    "revision": "5359643622cd017affcb318d8ae1e4e3",
+    "url": "/static/media/lowPolyMountains_main.53596436.jpg"
+  },
+  {
+    "revision": "14f52cd7711eecc24a4678b98f98647e",
+    "url": "/static/media/lowPolyMountains_thumb.14f52cd7.jpg"
+  },
+  {
+    "revision": "4285e2ee24d5b8d1063f543aa1f0b87a",
+    "url": "/static/media/mariaopoly_thumb.4285e2ee.jpg"
+  },
+  {
+    "revision": "dae91b23b35cd7731e7f88d9b3801c6a",
+    "url": "/static/media/memorygame_thumb.dae91b23.jpg"
+  },
+  {
+    "revision": "55688c898eb697f3ef598a113134ea8f",
+    "url": "/static/media/mp01.55688c89.jpg"
+  },
+  {
+    "revision": "c865ecd228c2e29fd7f9854e4830ca58",
+    "url": "/static/media/mp02.c865ecd2.jpg"
+  },
+  {
+    "revision": "4f2052ace61cb58c0ac2fcceb612b8a0",
+    "url": "/static/media/mp03.4f2052ac.jpg"
+  },
+  {
+    "revision": "607adca22df3213df86c22947fef0ae0",
+    "url": "/static/media/mp04.607adca2.jpg"
+  },
+  {
+    "revision": "fafb566f423f7f8a399d4def9e4233cc",
+    "url": "/static/media/mp05.fafb566f.jpg"
+  },
+  {
+    "revision": "7fdf1f22b49ce3075d0c824747281358",
+    "url": "/static/media/restaurantsreview_thumb.7fdf1f22.jpg"
+  },
+  {
+    "revision": "423e9d36534f78293cd0da418359b9e1",
+    "url": "/static/media/thegameboy_thumb.423e9d36.jpg"
+  }
+]);
