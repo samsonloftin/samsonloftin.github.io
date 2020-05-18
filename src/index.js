c4673582ae008ad03e477ea29bcf54729981bb76
Samson <contact@samsonloftin.com>
2020-05-17 22:53:30 -0700

    AWS Amplify Added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
+import Amplify from "aws-amplify";
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
 
 ReactDOM.render(
-  <BrowserRouter basename='/'>
+  <HashRouter basename='/'>
     <App />
-  </BrowserRouter>, document.getElementById('root'));
+  </HashRouter>, document.getElementById('root'));
