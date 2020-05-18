1c4250c09080539f4ce45254f3d5e9da404cdbed
Samson <contact@samsonloftin.com>
2020-05-18 11:59:13 -0700

    Fixing AWS issues

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
-import Amplify from "aws-amplify";
-import awsExports from "./aws-exports";
-Amplify.configure(awsExports);
 
 ReactDOM.render(
   <BrowserRouter basename='/'>
